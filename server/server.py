1d78ac0dd3fdad46611e9b71490b53fe72e427b6
mmatuszewsky <maciasmax10@gmail.com>
2024-02-24 19:13:21 +0100

    add influxdb + remove depracated methods

--- server/server.py
+++ server/server.py
@@ -4,8 +4,25 @@ from rclpy import init, shutdown, spin
 from rclpy.node import Node
 from std_msgs.msg import Int32
 import threading
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+subscriber = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global subscriber
+    global spin_thread
+    init()
+    subscriber = NumberSubscriber()
+    spin_thread = threading.Thread(target=spin, args=(subscriber,))
+    spin_thread.start()
+    yield
+    if subscriber is not None:
+        subscriber.destroy_node()
+        spin_thread.join()
+    shutdown()
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,32 +41,12 @@ class NumberSubscriber(Node):
 
     def number_callback(self, msg):
         self.latest_number = msg.data
-        print(self.latest_number)
-
-subscriber = None
 
 @app.get("/latest-number")
-def get_latest_number():
+async def get_latest_number():
     if subscriber is None:
         raise HTTPException(status_code=500, detail="ROS not initialized")
     return {"latest_number": subscriber.latest_number}
-
-@app.on_event("startup")
-async def startup_event():
-    global subscriber
-    global spin_thread
-    init()
-    subscriber = NumberSubscriber()
-    spin_thread = threading.Thread(target=spin, args=(subscriber,))
-    spin_thread.start()
-   
-@app.on_event("shutdown")
-def shutdown_event():
-    global subscriber
-    if subscriber is not None:
-        subscriber.destroy_node()
-        spin_thread.join()
-    shutdown()
 
 if __name__ == "__main__":
     import uvicorn
