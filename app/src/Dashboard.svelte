096a1c4017a4b790537ee8cf4707051ae4301fee
mmatuszewsky <maciasmax10@gmail.com>
2024-02-26 17:42:42 +0100

    add config + some divs

--- app/src/Dashboard.svelte
+++ app/src/Dashboard.svelte
@@ -11,7 +11,7 @@
         {
             path.style.fill = "url(#gradient_green)";
         }
-        else if (path.id === 'valve1' || path.id === 'valve2')
+        else if (path.id === 'valve1' || path.id === 'valve2' || path.id === 'hatch')
         {
             path.style.stroke = "url(#gradient_red)";
         }
@@ -22,9 +22,44 @@
 </script>
 
 
-<div on:click={changeColor}>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:click={changeColor} id="svg-container">
     <SVG width=70vw />
+    <div id="div-elem">
+        Some text!
+    </div>
+    <div id="div-elem2">
+        Some text!
+    </div>
 </div>
 
 <style>
+    #svg-container {
+        position: relative;
+        width: 70vw;
+    }
+
+    #div-elem {
+      position: absolute;
+      top: 5%;
+      left: -25%;
+      width: 20%;
+      height: 88%;
+      border-radius: 1vw;
+      border: 1px solid #eee;
+      background-color: #242424;
+    }
+
+    #div-elem2 {
+      position: absolute;
+      top: 5%;
+      left: 10%;
+      width: 35%;
+      height: 50%;
+      border-radius: 1vw;
+      border: 1px solid #eee;
+      background-color: #242424;
+    }
+
 </style>
