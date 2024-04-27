2cd843d84cbdd0add061fe4f0995063b301dce8a
Maciej <maciejziolkowski21@gmail.com>
2024-04-28 00:37:35 +0200

    remove leftovers, state type of get_node_name explicitly

--- gs_ros2_utils/__init__.py
+++ gs_ros2_utils/__init__.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
  
-def get_node_name(message_type):
+def get_node_name(message_type: str):
     # this function assumes that every node has its own message type
     # Read config.json
     with open(get_config_path(), 'r') as f:
@@ -24,7 +24,6 @@ def get_config_path():
     while searched_dir.name != 'gs_web_app'  and i < 10: # search iter limit
         searched_dir = searched_dir.parent
         i += 1
-    i -=1 # adjust for last iteration
 
     # Check if we found the 'gs_web_app' directory
     if searched_dir.name != 'gs_web_app':
